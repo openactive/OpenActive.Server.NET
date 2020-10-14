37e924d0b9822e253ab868fb26ad4be1cc440014
Nemanja Stefanovic <nemanja.stef011@gmail.com>
2020-10-14 17:30:35 +0200

    - added support for SellerRequestedCancellationSimulateAction

--- Fakes/OpenActive.FakeDatabase.NET/FakeBookingSystem.cs
+++ Fakes/OpenActive.FakeDatabase.NET/FakeBookingSystem.cs
@@ -491,7 +491,17 @@ namespace OpenActive.FakeDatabase.NET
         {
             using (var db = Mem.Database.Open())
             {
-                var order = db.Single<OrderTable>(x => x.ClientId == clientId && x.OrderMode == OrderMode.Booking && x.OrderId == uuid && !x.Deleted);
+                OrderTable order = null;
+                if (customerCancelled)
+                {
+                    order = db.Single<OrderTable>(x => x.ClientId == clientId && x.OrderMode == OrderMode.Booking && x.OrderId == uuid && !x.Deleted);
+                }
+                else
+                {
+                    // When seller cancels only uuid is sent.
+                    order = db.Single<OrderTable>(x => x.OrderId == uuid && !x.Deleted);
+                }
+
                 if (order != null)
                 {
                     if (sellerId.HasValue && order.SellerId != sellerId)
@@ -499,7 +509,19 @@ namespace OpenActive.FakeDatabase.NET
                         throw new ArgumentException("SellerId does not match Order");
                     }
                     List<OrderItemsTable> updatedOrderItems = new List<OrderItemsTable>();
-                    foreach (OrderItemsTable orderItem in db.Select<OrderItemsTable>(x => x.ClientId == clientId && x.OrderId == order.OrderId && orderItemIds.Contains(x.Id)))
+
+                    List<OrderItemsTable> orderItems = null;
+
+                    if (customerCancelled)
+                    {
+                        orderItems = db.Select<OrderItemsTable>(x => x.ClientId == clientId && x.OrderId == order.OrderId && orderItemIds.Contains(x.Id));
+                    }
+                    else
+                    {
+                        orderItems = db.Select<OrderItemsTable>(x => x.OrderId == order.OrderId);
+                    }
+
+                    foreach (OrderItemsTable orderItem in orderItems)
                     {
                         if (orderItem.Status == BookingStatus.Confirmed || orderItem.Status == BookingStatus.Attended)
                         {
