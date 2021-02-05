ea510b21a74c9313500cd3883f9eb15bba67d86b
NemanjaStefanovic <nemanja.stef011@gmail.com>
2021-02-05 16:54:14 +0100

    Coverage/reseller and nobroker role (#74)
    * - moving check from storebookingEngine to custom bookingEnginge
    
    * - adding new checks for nobroker role and reseller broker role
    
    * fix some warnings
    
    * fix reseller logic and fix some warnings
    
    * fix logic for customer details validation
    
    * slight refactor of logic
    
    * add reseller logic to StoreBookingEngine
    
    * fix IncompleteCustomerDetailsError logic
    
    Co-authored-by: Luke Winship <luke.winship@googlemail.com>
    Co-authored-by: Civ Sivakumaran <civ@imin.co>

--- Examples/BookingSystem.AspNetCore/Stores/OrderStore.cs
+++ Examples/BookingSystem.AspNetCore/Stores/OrderStore.cs
@@ -162,7 +162,7 @@ namespace BookingSystem
                 flowContext.BrokerRole == BrokerType.AgentBroker ? BrokerRole.AgentBroker : flowContext.BrokerRole == BrokerType.ResellerBroker ? BrokerRole.ResellerBroker : BrokerRole.NoBroker,
                 flowContext.Broker.Name,
                 flowContext.SellerId.SellerIdLong ?? null, // Small hack to allow use of FakeDatabase when in Single Seller mode
-                flowContext.Customer.Email,
+                flowContext.Customer?.Email,
                 flowContext.Payment?.Identifier,
                 responseOrder.TotalPaymentDue.Price.Value,
                 databaseTransaction.FakeDatabaseTransaction,
@@ -184,7 +184,7 @@ namespace BookingSystem
                 flowContext.BrokerRole == BrokerType.AgentBroker ? BrokerRole.AgentBroker : flowContext.BrokerRole == BrokerType.ResellerBroker ? BrokerRole.ResellerBroker : BrokerRole.NoBroker,
                 flowContext.Broker.Name,
                 flowContext.SellerId.SellerIdLong ?? null, // Small hack to allow use of FakeDatabase when in Single Seller mode
-                flowContext.Customer.Email,
+                flowContext.Customer?.Email,
                 flowContext.Payment?.Identifier,
                 responseOrderProposal.TotalPaymentDue.Price.Value,
                 databaseTransaction.FakeDatabaseTransaction,
