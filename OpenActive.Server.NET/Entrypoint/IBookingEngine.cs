e310060213557e2d41dcfadea85996ef9ff5ac96
Civ Sivakumaran <civ@imin.co>
2021-04-19 13:07:12 +0100

    fully asynced OrderProposalUpdate

--- OpenActive.Server.NET/Entrypoint/IBookingEngine.cs
+++ OpenActive.Server.NET/Entrypoint/IBookingEngine.cs
@@ -30,7 +30,7 @@ namespace OpenActive.Server.NET
         ResponseContent DeleteOrder(string clientId, Uri sellerId, string uuid);
         ResponseContent DeleteOrderQuote(string clientId, Uri sellerId, string uuid);
         ResponseContent ProcessOrderUpdate(string clientId, Uri sellerId, string uuid, string orderJson);
-        ResponseContent ProcessOrderProposalUpdate(string clientId, Uri sellerId, string uuid, string orderJson);
+        Task<ResponseContent> ProcessOrderProposalUpdate(string clientId, Uri sellerId, string uuid, string orderJson);
 
         // These endpoints are authenticated by client credentials (OAuth Client Credentials Grant)
         ResponseContent InsertTestOpportunity(string testDatasetIdentifier, string eventJson);
