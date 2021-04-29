8d0dcb0d5b7e93f8c4be29465e7a675e78b4cb98
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2021-04-29 12:53:00 +0100

    support OrderProposals feed

--- Examples/BookingSystem.AspNetFramework/Feeds/OrdersFeed.cs
+++ Examples/BookingSystem.AspNetFramework/Feeds/OrdersFeed.cs
@@ -14,38 +14,22 @@ namespace BookingSystem
     {
         //public override string FeedPath { get; protected set; } = "example path override";
 
-        protected async override Task<List<RpdeItem>> GetRPDEItems(OrderType feedType, string clientId, long? afterTimestamp, string afterId)
+        protected async override Task<List<RpdeItem>> GetRPDEItems(string clientId, long? afterTimestamp, string afterId)
         {
-            if (!(feedType == OrderType.Order || feedType == OrderType.OrderProposal)) throw new ArgumentOutOfRangeException(nameof(feedType));
             using (var db = FakeBookingSystem.Database.Mem.Database.Open())
             {
-                // Note two queries are only required when implementing the Approval flow, otherwise only an Orders feed is required
-                var q = (feedType == OrderType.Order ?
-                    db.From<OrderTable>()
-                    .LeftJoin<OrderTable, OrderItemsTable>((orders, items) => orders.OrderId == items.OrderId)
-                    .OrderBy(x => x.OrderModified)
-                    .ThenBy(x => x.OrderId)
-                    .Where(x =>
-                        x.VisibleInOrdersFeed != FeedVisibility.None && x.ClientId == clientId && (
-                            !afterTimestamp.HasValue ||
-                            x.OrderModified > afterTimestamp ||
-                            x.OrderModified == afterTimestamp &&
-                            string.Compare(afterId, x.OrderId, StringComparison.InvariantCulture) > 0) &&
-                        x.OrderModified < (DateTimeOffset.UtcNow - new TimeSpan(0, 0, 2)).UtcTicks)
-                    .Take(RPDEPageSize)
-                    :
-                    db.From<OrderTable>()
-                    .LeftJoin<OrderTable, OrderItemsTable>((orders, items) => orders.OrderId == items.OrderId)
-                    .OrderBy(x => x.OrderProposalModified)
-                    .ThenBy(x => x.OrderId)
-                    .Where(x =>
-                        x.VisibleInOrderProposalsFeed != FeedVisibility.None && x.ClientId == clientId && (
-                            !afterTimestamp.HasValue ||
-                            x.OrderProposalModified > afterTimestamp ||
-                            x.OrderProposalModified == afterTimestamp &&
-                            string.Compare(afterId, x.OrderId, StringComparison.InvariantCulture) > 0) &&
-                        x.OrderProposalModified < (DateTimeOffset.UtcNow - new TimeSpan(0, 0, 2)).UtcTicks)
-                    .Take(RPDEPageSize));
+                var q = db.From<OrderTable>()
+                .LeftJoin<OrderTable, OrderItemsTable>((orders, items) => orders.OrderId == items.OrderId)
+                .OrderBy(x => x.OrderModified)
+                .ThenBy(x => x.OrderId)
+                .Where(x =>
+                    x.VisibleInOrdersFeed != FeedVisibility.None && x.ClientId == clientId && (
+                        !afterTimestamp.HasValue ||
+                        x.OrderModified > afterTimestamp ||
+                        x.OrderModified == afterTimestamp &&
+                        string.Compare(afterId, x.OrderId, StringComparison.InvariantCulture) > 0) &&
+                    x.OrderModified < (DateTimeOffset.UtcNow - new TimeSpan(0, 0, 2)).UtcTicks)
+                .Take(RPDEPageSize);
 
                 var query = db
                     .SelectMulti<OrderTable, OrderItemsTable>(q)
@@ -103,10 +87,65 @@ namespace BookingSystem
                                         orderItem.Status == BookingStatus.Confirmed ? OrderItemStatus.OrderItemConfirmed :
                                         orderItem.Status == BookingStatus.CustomerCancelled ? OrderItemStatus.CustomerCancelled :
                                         orderItem.Status == BookingStatus.SellerCancelled ? OrderItemStatus.SellerCancelled :
-                                        orderItem.Status == BookingStatus.Attended ? OrderItemStatus.CustomerAttended :
-                                        orderItem.Status == BookingStatus.Proposed ? OrderItemStatus.OrderItemProposed : (OrderItemStatus?)null,
+                                        orderItem.Status == BookingStatus.Attended ? OrderItemStatus.CustomerAttended : (OrderItemStatus?)null,
                                     CancellationMessage = orderItem.CancellationMessage,
                                     CustomerNotice = orderItem.CustomerNotice,
+                                }).ToList()
+                            )
+                    });
+
+                return query.ToList();
+            }
+        }
+    }
+
+    public class AcmeOrderProposalsFeedRpdeGenerator : OrdersRPDEFeedModifiedTimestampAndID
+    {
+        //public override string FeedPath { get; protected set; } = "example path override";
+
+        protected async override Task<List<RpdeItem>> GetRPDEItems(string clientId, long? afterTimestamp, string afterId)
+        {
+            using (var db = FakeBookingSystem.Database.Mem.Database.Open())
+            {
+                var q = db.From<OrderTable>()
+                .LeftJoin<OrderTable, OrderItemsTable>((orders, items) => orders.OrderId == items.OrderId)
+                .OrderBy(x => x.OrderProposalModified)
+                .ThenBy(x => x.OrderId)
+                .Where(x =>
+                    x.VisibleInOrderProposalsFeed != FeedVisibility.None && x.ClientId == clientId && (
+                        !afterTimestamp.HasValue ||
+                        x.OrderProposalModified > afterTimestamp ||
+                        x.OrderProposalModified == afterTimestamp &&
+                        string.Compare(afterId, x.OrderId, StringComparison.InvariantCulture) > 0) &&
+                    x.OrderProposalModified < (DateTimeOffset.UtcNow - new TimeSpan(0, 0, 2)).UtcTicks)
+                .Take(RPDEPageSize);
+
+                var query = db
+                    .SelectMulti<OrderTable, OrderItemsTable>(q)
+                    .GroupBy(x => new { x.Item1.OrderId })
+                    .Select(result => new
+                    {
+                        OrderTable = result.Select(item => new { item.Item1 }).FirstOrDefault()?.Item1,
+                        OrderItemsTable = result.Select(item => new { item.Item2 }).ToList().Select(orderItem => orderItem.Item2).ToList()
+                    })
+                    .Select(result => new RpdeItem
+                    {
+                        Kind = RpdeKind.Order,
+                        Id = result.OrderTable.OrderId,
+                        Modified = result.OrderTable.OrderProposalModified,
+                        State = result.OrderTable.Deleted || result.OrderTable.VisibleInOrderProposalsFeed == FeedVisibility.Archived ? RpdeState.Deleted : RpdeState.Updated,
+                        Data = result.OrderTable.Deleted || result.OrderTable.VisibleInOrderProposalsFeed == FeedVisibility.Archived ? null :
+                            AcmeOrderStore.RenderOrderFromDatabaseResult(RenderOrderId(result.OrderTable.OrderMode == OrderMode.Proposal ? OrderType.OrderProposal : OrderType.Order, result.OrderTable.OrderId), result.OrderTable,
+                                result.OrderItemsTable.Select(orderItem => new OrderItem
+                                {
+                                    Id = RenderOrderItemId(OrderType.Order, result.OrderTable.OrderId, orderItem.Id),
+                                    AcceptedOffer = new Offer
+                                    {
+                                        Id = orderItem.OfferJsonLdId,
+                                        Price = orderItem.Price,
+                                        PriceCurrency = "GBP"
+                                    },
+                                    OrderedItem = orderItem.OpportunityJsonLdId
                                 }).ToList()
                             )
                     });
