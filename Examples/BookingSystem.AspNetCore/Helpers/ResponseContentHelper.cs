8382e22d485f1b4a97c4285383cffda789b6c0c0
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2021-09-30 22:12:57 +0100

    Revert "feat: RPDE cache headers (#163)"
    This reverts commit e73eee35d98025f3ed6db69d5a490c4864601f4c.

--- Examples/BookingSystem.AspNetCore/Helpers/ResponseContentHelper.cs
+++ Examples/BookingSystem.AspNetCore/Helpers/ResponseContentHelper.cs
@@ -1,46 +1,15 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace BookingSystem.AspNetCore.Helpers
+﻿namespace BookingSystem.AspNetCore.Helpers
 {
     public static class ResponseContentHelper
     {
         public static Microsoft.AspNetCore.Mvc.ContentResult GetContentResult(this OpenActive.Server.NET.OpenBookingHelper.ResponseContent response)
         {
-            return new CacheableContentResult
+            return new Microsoft.AspNetCore.Mvc.ContentResult
             {
                 StatusCode = (int)response.StatusCode,
                 Content = response.Content,
-                ContentType = response.ContentType,
-                CacheControlMaxAge = response.CacheControlMaxAge,
+                ContentType = response.ContentType
             };
-        }
-    }
-
-    /// <summary>
-    /// ContentResult that also sets Cache-Control: public, max-age=X, s-maxage=X
-    /// See https://developer.openactive.io/publishing-data/data-feeds/scaling-feeds for more information
-    /// </summary>
-    public class CacheableContentResult : Microsoft.AspNetCore.Mvc.ContentResult
-    {
-        public TimeSpan CacheControlMaxAge { get; set; }
-
-        public override async Task ExecuteResultAsync(ActionContext context)
-        {
-            if (CacheControlMaxAge != null)
-            {
-                context.HttpContext.Response.GetTypedHeaders().CacheControl =
-                    new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
-                    {
-                        Public = true,
-                        MaxAge = CacheControlMaxAge,
-                        SharedMaxAge = CacheControlMaxAge,
-                    };
-            }
-
-            await base.ExecuteResultAsync(context);
         }
     }
 }
