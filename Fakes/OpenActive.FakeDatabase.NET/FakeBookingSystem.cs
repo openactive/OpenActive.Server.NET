6b9724c597ace69a6e1ca10063bf6712bcf956bb
Civ Sivakumaran <civ@imin.co>
2023-06-14 13:28:47 +0100

    fix tests

--- Fakes/OpenActive.FakeDatabase.NET/FakeBookingSystem.cs
+++ Fakes/OpenActive.FakeDatabase.NET/FakeBookingSystem.cs
@@ -41,17 +41,6 @@ namespace OpenActive.FakeDatabase.NET
         //            BookingPartnerTable.Get().Wait();
         //#pragma warning restore 4014
         //        }
-
-        //public static DateTime Truncate(this DateTime dateTime, TimeSpan timeSpan)
-        //{
-        //    if (timeSpan == TimeSpan.Zero)
-        //        return dateTime; // Or could throw an ArgumentException
-
-        //    if (dateTime == DateTime.MinValue || dateTime == DateTime.MaxValue)
-        //        return dateTime; // do not modify "guard" values
-
-        //    return dateTime.AddTicks(-(dateTime.Ticks % timeSpan.Ticks));
-        //}
     }
 
     /// <summary>
